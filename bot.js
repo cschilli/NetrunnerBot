894b0d874ae079c8c7231fc58ff291ee86f615b3
Christopher Schilling <cschilli@uno.edu>
2017-11-29 14:50:13 -0600

    Fleshed out API

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xmlHttp = new XMLHttpRequest();
+
+
+
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -18,23 +23,69 @@ bot.on('ready', function (evt) {
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
+        // args becomes everything from index 1 onwards split by spaces
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-
         args = args.splice(1);
+
+        function findCard(args){
+            for(var i = 0; i < response.length; i++)
+            {
+                if(response.data[i].title == args)
+                {
+                    return response.data[i].code;
+                }
+            }
+        }
+
         switch(cmd) {
             // !ping
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
-                });
+                })
+                break;// Just add any case commands if you want to..
+            case 'hey':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Hey cool dude!'
+                })
                 break;
-            // Just add any case commands if you want to..
+            case 'discourage':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'You are smelly, ' + args[0] + '!'
+                })
+                break;
+            case 'encourage':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'You are simply the best, ' + args[0] + '!'
+                })
+                break;
+            case 'card':
+                xmlHttp.open("GET", "https://netrunnerdb.com/api/2.0/public/cards", false);
+                xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                xmlHttp.send();
+                var response = JSON.parse(xmlHttp.responseText);
+                //console.log(response);
+
+                var cardName = args[0];
+                var cardCode = findCard(cardName);
+                console.log(cardCode);
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'https://netrunnerdb.com/card_image/' + cardCode + '.png'
+                })
+                break;
+
         }
     }
 });
